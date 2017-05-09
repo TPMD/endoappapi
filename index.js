aed67f50b3f7080580e0783abd54fc7eaa31c979
Jon Kolman <jonathankolman@gmail.com>
2017-05-09 02:14:46 +0200

    renamed app.js to main.js as i believe aws is running node app.js before npm start

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 require('babel-register')
 require('./config')
-require('./app.js')
+require('./main.js')
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ import doctorRouter from './doctor'
 import patientRouter from './patient'
 import userRouter from './user'
 import imageRouter from './images'
+import testRouter from './test'
 
 let router = Router()
 router.use('/user', userRouter)
 router.use('/patient', patientRouter)
 router.use('/doctor', doctorRouter)
 router.use('/images', imageRouter)
+router.use('/test', testRouter)
 export default router
